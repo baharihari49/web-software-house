d86b3a5304185e38676cbcbdd3c83aa819b3db42
bahari <haribahari71@gmail.com>
2025-04-12 07:22:44 +0700

    fix fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 
-const poppins = Poppins({
+// Setup font Roboto
+const roboto = Roboto({
   subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-  variable: "--font-poppins",
+  weight: ["300", "400", "500", "700", "900"],
+  variable: "--font-roboto", // opsional jika pakai CSS variable
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -22,8 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="id" className="scroll-smooth">
-      <body className={`${poppins.variable} font-sans`}>
+    <html lang="id" className={`${roboto.className} scroll-smooth`}>
+      <body>
         <Header />
         <main>{children}</main>
         <Footer />
